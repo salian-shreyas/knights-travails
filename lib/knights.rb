b2d21c846098a82a6cb1026e3e67196f61479af6
Shreyas G Salian <69745409+salian-shreyas@users.noreply.github.com>
2025-05-10 04:24:52 +0530

    Implement short-path search algorithm

--- lib/knights.rb
+++ lib/knights.rb
@@ -1,26 +1,55 @@
 # class for knights path
 class Knights
-  def initialize
-    @board = build_board
+  def initialize(coordinates)
+    @coordinates = coordinates
+    @short_path = []
+  end
+
+  def path(destination)
+    search_path(destination)
+    print_path
   end
 
   private
 
-  def build_board
-    @board = Array.new(64)
+  def print_path
+    p @short_path
+  end
 
-    @board.each_index do |idx|
-      current_square = Square.new(square_coordinates(idx))
-      board[idx] = immediate_squares(current_square)
+  def search_path(destination, start = @coordinates, current_path = [])
+    return if longer_path?(current_path)
+
+    current_path << start
+    if destination_reached?(start, destination)
+      @short_path = current_path
+    else
+      search_immediate_path(destination, start, current_path)
     end
   end
 
+  def search_immediate_path(destination, start, current_path)
+    immediate_squares(Square.new(start)).each do |square|
+      search_path(destination, square.data, current_path.dup) unless current_path.include? square.data
+    end
+  end
+
+  def longer_path?(current_path)
+    return false if @short_path.empty?
+
+    current_path.length >= @short_path.length
+  end
+
+  def destination_reached?(start, destination)
+    start == destination
+  end
+
   def immediate_squares(square)
+    squares = []
     possible_moves(square).each do |move|
-      square.next = Square.new(move, square.next) if valid?(move)
+      squares << Square.new(move, square.next) if valid?(move)
     end
 
-    square
+    squares
   end
 
   def possible_moves(square)
@@ -41,22 +70,5 @@ class Knights
 
   def valid?(move)
     move[0].between?(0, 7) && move[1].between?(0, 7)
-  end
-
-  def square_coordinates(idx)
-    row = case idx
-          when 0..7 then 0
-          when 8..15 then 1
-          when 16..23 then 2
-          when 24..31 then 3
-          when 32..39 then 4
-          when 40..47 then 5
-          when 48..55 then 6
-          else 7
-          end
-
-    col = idx % 8
-
-    [row, col]
   end
 end
--- main.rb
+++ main.rb
@@ -1,4 +1,6 @@
+require "pry-byebug"
 require_relative "lib/square"
 require_relative "lib/knights"
 
-knights = Knights.new
+knights = Knights.new([3, 3])
+knights.path([4, 3])
